4781096b2e0a5a261f36c156000fb07e234f802e
Wiktor Wielgusiak <82612654+WIKTORWIELGUSIAK@users.noreply.github.com>
2024-05-14 13:15:38 +0200

    Delete app/helpers/handleAnimalAddition.ts

--- app/helpers/handleAnimalAddition.ts
+++
@@ -1,8 +0,0 @@
-export const handleAnimalAddition = (result: string) => {
-  if (
-    ["rabbit", "sheep", "pig", "cow", "horse"].includes(result) &&
-    (playerData.animals[result] > 0 || result1 === result2)
-  ) {
-    playerData.animals[result] += 1;
-  }
-};
